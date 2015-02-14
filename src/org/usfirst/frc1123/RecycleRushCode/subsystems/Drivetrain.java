bef8da79bf4f55dfa9ef091d0aa96a7111a942f9
bluedot951 <gore.shantanu@gmail.com>
2015-02-14 16:55:12 -0500

    made branch for tank

--- src/org/usfirst/frc1123/RecycleRushCode/subsystems/Drivetrain.java
+++ src/org/usfirst/frc1123/RecycleRushCode/subsystems/Drivetrain.java
@@ -35,8 +35,8 @@ public class Drivetrain extends Subsystem {
     	
 //       	if(Math.abs(y) >= Math.abs(x)) {
        		
-       		double leftVal = (-1 * y + twist) * 0.3;
-       		double rightVal = (y + twist) * 0.3;
+       		double leftVal = (-1 * y + twist) * 0.7;
+       		double rightVal = (y + twist) * 0.7;
        		       		
        		if(rightVal > 1) {
        			leftVal /= rightVal;
@@ -71,44 +71,6 @@ public class Drivetrain extends Subsystem {
     	
     	System.out.println(s.getTwist());
     	
-//    	double angle = s.getDirectionRadians();
-//    	double magnitude = s.getMagnitude();
-//    	
-//    	
-//    	System.out.println(magnitude + "\t" + angle);
-//    	
-//    	double sinangle = Math.sin(angle);
-//    	double cosangle = Math.cos(angle);
-//    	
-//    	if(magnitude < 0.1) {
-//    		stop();
-//    	}
-//    	
-//    	if(Math.abs(sinangle) > Math.abs(cosangle)) {
-//    		if(cosangle > 0) {
-//    			hDrive.set(magnitude);
-//    		}
-//    		
-//    		else {
-//    			hDrive.set(-1 * magnitude);
-//    		}
-//    	}
-//
-//	
-//    	else {
-//    		
-//    		if(sinangle > 0) {
-//	    		leftDrive.set(magnitude);
-//	    		rightDrive.set(-1 * magnitude);
-//    		}
-//    		
-//    		else {
-//	    		leftDrive.set(-1 * magnitude);
-//	    		rightDrive.set(magnitude);
-//    		}
-//	    	
-//	    	
-//	    }
    	}
 }
 
